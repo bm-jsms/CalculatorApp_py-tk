a0a5ada5f4b6bd92e0012642380e116ebb848036
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-08 18:48:49 -0500

    rm - geometry

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ root = tk.Tk()
 class Calculator:
     def __init__(self, main):
         self.main = main
-        self.display = tk.Entry(main, width=15, font=("Arial", 16), bg="#000105", fg="white", borderwidth=2)
+        self.display = tk.Entry(main, width=15, font=(
+            "Arial", 16), bg="#000105", fg="white", borderwidth=2)
         self.display.grid(row=0, column=0)
 
 
-root.geometry("400x600")
 root.title("Calculator")
 
 my_calc = Calculator(root)
