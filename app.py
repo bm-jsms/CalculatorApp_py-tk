aa1a9c8f231afe0a13f64b15b17f9180b6a24d72
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-08 18:51:31 -0500

    up - colors

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ root = tk.Tk()
 class Calculator:
     def __init__(self, main):
         self.main = main
-        self.display = tk.Entry(main, width=15, font=(
-            "Arial", 16), bg="#000105", fg="white", borderwidth=2)
+        self.display = tk.Entry(main, width=15, font=( "Arial", 16), bg="#01145d", fg="white", bd=10, insertwidth=1)
         self.display.grid(row=0, column=0)
 
 
