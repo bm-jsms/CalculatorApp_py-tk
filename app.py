944d24acee8f6c44c01a3739f0ae0856f21175bd
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-08 18:48:25 -0500

    class Calculator

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ import tkinter as tk
 
 root = tk.Tk()
 
+
+class Calculator:
+    def __init__(self, main):
+        self.main = main
+        self.display = tk.Entry(main, width=15, font=("Arial", 16), bg="#000105", fg="white", borderwidth=2)
+        self.display.grid(row=0, column=0)
+
+
 root.geometry("400x600")
 root.title("Calculator")
+
+my_calc = Calculator(root)
+
 
 root.mainloop()
